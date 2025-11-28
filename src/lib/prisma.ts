c378829c50b03565730d41abc7086dc6d3c2a584
dennys-terra <dennys.jornadadev@gmail.com>
2025-11-28 20:55:13 -0300

    feat: add migration unique constraint of  slug for restaurant

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+declare global {
+  var cachedPrisma: PrismaClient;
+}
+
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prisma = global.cachedPrisma;
+}
+
+// vou usar para chamar meu banco de dados
+export const db = prisma;
