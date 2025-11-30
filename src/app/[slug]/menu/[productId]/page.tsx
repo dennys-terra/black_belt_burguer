66f6e6a6f3c9fd23a6b4a7aea954c253e450849f
dennys-terra <dennys.jornadadev@gmail.com>
2025-11-30 00:11:07 -0300

    feat: add page detailsProductPage

--- src/app/[slug]/menu/[productId]/page.tsx
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -2,7 +2,8 @@ import { notFound } from "next/navigation"
 
 import { db } from "@/lib/prisma"
 
-import DetailsProductHeader from "./components/DetailsProductHeader"
+import DetailsProduct from "./components/DetailsProduct"
+import ProductHeader from "./components/ProductHeader"
 
 type DetailsProductPageProps = {
     params: Promise<{
@@ -12,16 +13,22 @@ type DetailsProductPageProps = {
 }
 
 const detailsProductPage = async ({params} : DetailsProductPageProps) => {
-    const {slug, productId} = await params
-    const product = await db.product.findUnique({where: {id: productId}})
+    const { productId, slug} = await params
+    
+    const product = await db.product.findUnique({where: {id: productId}, include: {restaurant: {select:{name: true, avatarImageUrl: true , slug: true}}}})
     if(!product){
         return notFound()
     }
+
+    if(product.restaurant.slug.toUpperCase() !== slug.toUpperCase()) {
+        return notFound()
+    }
+
     return ( 
-        <>
-            <DetailsProductHeader product={product}/>
-            <>{slug}, {productId}</>
-        </>
+        <div className="flex h-full flex-col">
+            <ProductHeader product={product}/>
+            <DetailsProduct product={product} />
+        </div>
      );
 }
  
