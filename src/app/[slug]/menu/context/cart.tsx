2a6ad6cb2620794b986b156a99d5f636ed1757d0
dennys-terra <dennys.jornadadev@gmail.com>
2025-12-07 23:31:54 -0300

    feat: ensure same product can be added more than once to cart

--- src/app/[slug]/menu/context/cart.tsx
+++ src/app/[slug]/menu/context/cart.tsx
@@ -4,6 +4,7 @@ import { Product } from "@prisma/client"
 import { createContext, ReactNode, useState} from "react"
 
 
+
 interface CartProduct extends Pick<Product, | 'id' | 'name' | 'price' | 'imageUrl' >  {
     quantity: number
 }
@@ -31,8 +32,20 @@ export const CartProvider = ({children}: {children: ReactNode}) => {
     }
 
     const addProduct = (product : CartProduct) => {
-        setProducts(prev => ([...prev , product]) )
-    }
+       const productIsAlreadyOnTheCart = products.some(prevProduct => prevProduct.id === product.id )
+       if(!productIsAlreadyOnTheCart){
+        return setProducts([...products, product]) 
+       }
+        setProducts((prevProducts) => {
+        return prevProducts.map(prevProduct => {
+            if(prevProduct.id === product.id){
+                return {...prevProduct, quantity: prevProduct.quantity + product.quantity}
+            }
+            return prevProduct
+        })
+       } )
+            
+        }
 
     return (
         <CartContext.Provider value={{
